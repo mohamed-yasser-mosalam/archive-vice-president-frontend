4827224943f14823e3caa7bedad5b12a3ef7f6d7
esraaelyan <esraaelyan2002@gmail.com>
2024-03-22 00:18:58 +0200

    updateing

--- src/app/Models/userActivity/user-activity.ts
+++ src/app/Models/userActivity/user-activity.ts
@@ -5,16 +5,20 @@ export class UserActivity {
   lastModifiedBy:Date;
   createdDate:Date;
   lastModifiedDate:Date;
-  type:string
+  type:string;
+  no:number;
+
 
   constructor(id: number, summary: string, createdBy: string, lastModifiedBy: Date,
-              createdDate:Date,lastModifiedDate:Date,type:string) {
+              createdDate:Date,lastModifiedDate:Date,type:string,no:number
+) {
     this.id = id;
     this.summary = summary;
     this.createdBy = createdBy;
     this.lastModifiedBy = lastModifiedBy;
     this.createdDate=createdDate;
     this.lastModifiedDate=lastModifiedDate;
-    this.type=type
+    this.type=type;
+    this.no=no
   }
 }

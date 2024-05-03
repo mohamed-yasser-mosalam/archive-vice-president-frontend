ed4c707dcafa525eaa3484a5add805664f51308d
esraaelyan <esraaelyan2002@gmail.com>
2024-05-03 05:49:05 +0300

    DashBoard

--- src/app/update-special/update-special.component.ts
+++ src/app/update-special/update-special.component.ts
@@ -1,9 +1,88 @@
-import {Component, OnInit} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
-import {ActivatedRoute, Router} from "@angular/router";
-import {SpecialService} from "../Services/SpecialService/special.service";
-import {Special} from "../special";
-
+// import {Component, OnInit} from '@angular/core';
+// import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
+// import {ActivatedRoute, Router} from "@angular/router";
+// import {SpecialService} from "../Services/SpecialService/special.service";
+// import {Special} from "../special";
+//
+//
+// @Component({
+//   selector: 'app-update-special',
+//   templateUrl: './update-special.component.html',
+//   styleUrls: ['./update-special.component.css']
+// })
+// export class UpdateSpecialComponent implements OnInit {
+//   x: any
+//   form: FormGroup;
+//   page = this.routes.snapshot.params['id'];
+//   paths: []
+//   special: Special;
+//
+//
+//   constructor(private fb: FormBuilder, private specialService: SpecialService, private router: Router,
+//               private routes: ActivatedRoute) {
+//   }
+//
+//   getSpecial() {
+//     this.specialService.getSpecialsById(this.page).subscribe((res: any) => {
+//       this.special = res
+//     });
+//   }
+//
+//
+//
+//   createSubject() {
+//     return this.fb.group({
+//       num: [''],
+//       head: [''],
+//       decision: this.fb.array([this.createDecision()])
+//     });
+//   }
+//
+//   createDecision() {
+//     return this.fb.group({
+//       num: [''],
+//       summary: ['']
+//     });
+//   }
+//
+//
+//   get subjects() {
+//     return this.form.get('subjects') as FormArray;
+//   }
+//
+//   addSubject() {
+//     this.subjects.push(this.createSubject());
+//   }
+//
+//   deleteSubject(index: number) {
+//     this.subjects.removeAt(index);
+//   }
+//
+//   getDecisions(subjectIndex: number) {
+//     return (this.subjects.at(subjectIndex).get('decision') as FormArray);
+//   }
+//
+//   addDecision(subjectIndex: number) {
+//     this.getDecisions(subjectIndex).push(this.createDecision());
+//   }
+//
+//   deleteDecision(subjectIndex: number, decisionIndex: number) {
+//     this.getDecisions(subjectIndex).removeAt(decisionIndex);
+//   }
+//
+//
+//
+//   ngOnInit(): void {
+//      this.getSpecial()
+//    }
+//
+// }
+//
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { SpecialService } from "../Services/SpecialService/special.service";
+import { Special } from "../special";
 
 @Component({
   selector: 'app-update-special',
@@ -11,70 +90,90 @@ import {Special} from "../special";
   styleUrls: ['./update-special.component.css']
 })
 export class UpdateSpecialComponent implements OnInit {
-  x: any
   form: FormGroup;
   page = this.routes.snapshot.params['id'];
-  paths: []
   special: Special;
 
-
   constructor(private fb: FormBuilder, private specialService: SpecialService, private router: Router,
-              private routes: ActivatedRoute) {
+              private routes: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.getSpecial();
+    this.initializeForm();
   }
 
-  getSpecial() {
+  initializeForm(): void {
+    this.form = this.fb.group({
+      sender: [''],
+      num: [''],
+      summary: [''],
+      subjects: this.fb.array([])
+    });
+  }
+
+  getSpecial(): void {
     this.specialService.getSpecialsById(this.page).subscribe((res: any) => {
-      this.special = res
+      this.special = res;
+      this.patchFormValues();
     });
   }
 
+  patchFormValues(): void {
+    this.form.patchValue({
+      sender: this.special.sender,
+      num: this.special.num,
+      summary: this.special.summary
+    });
+    this.special.subjects.forEach(subject => {
+      this.addSubject(subject);
+    });
+  }
 
-
-  createSubject() {
+  createSubject(subject?: any): FormGroup {
     return this.fb.group({
-      num: [''],
-      head: [''],
-      decision: this.fb.array([this.createDecision()])
+      head: [subject ? subject.head : ''],
+      num: [subject ? subject.num : ''],
+      decisions: this.fb.array([])
     });
   }
 
-  createDecision() {
+  createDecision(decision?: any): FormGroup {
     return this.fb.group({
-      num: [''],
-      summary: ['']
+      summary: [decision ? decision.summary : ''],
+      num: [decision ? decision.num : '']
     });
   }
 
-
-  get subjects() {
+  get subjects(): FormArray {
     return this.form.get('subjects') as FormArray;
   }
 
-  addSubject() {
-    this.subjects.push(this.createSubject());
+  addSubject(subject?: any): void {
+    this.subjects.push(this.createSubject(subject));
+    if (subject && subject.decisions) {
+      subject.decisions.forEach(decision => {
+        this.addDecision(this.subjects.length - 1, decision);
+      });
+    }
   }
 
-  deleteSubject(index: number) {
+  deleteSubject(index: number): void {
     this.subjects.removeAt(index);
   }
 
-  getDecisions(subjectIndex: number) {
-    return (this.subjects.at(subjectIndex).get('decision') as FormArray);
+  getDecisions(subjectIndex: number): FormArray {
+    return this.subjects.at(subjectIndex).get('decisions') as FormArray;
   }
 
-  addDecision(subjectIndex: number) {
-    this.getDecisions(subjectIndex).push(this.createDecision());
+  addDecision(subjectIndex: number, decision?: any): void {
+    this.getDecisions(subjectIndex).push(this.createDecision(decision));
   }
 
-  deleteDecision(subjectIndex: number, decisionIndex: number) {
+  deleteDecision(subjectIndex: number, decisionIndex: number): void {
     this.getDecisions(subjectIndex).removeAt(decisionIndex);
   }
 
-
-
-  ngOnInit(): void {
-     this.getSpecial()
-   }
-
+  onSubmit(): void {
+     console.log(this.form.value);
+  }
 }
-
--- src/app/url.ts
+++ src/app/url.ts
@@ -1,3 +1,4 @@
 
-let baseUrl='http://localhost:1200'
+// let baseUrl='http://localhost:1200'
+let baseUrl='http://192.168.1.7:1200'
 export default baseUrl;

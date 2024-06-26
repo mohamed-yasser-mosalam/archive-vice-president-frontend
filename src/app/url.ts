eaf809d5345828f2dbd08446b666aad7374df2bd
drmohamedezzat <k.m.ezzat80@gmail.com>
2024-06-26 09:55:16 +0200

    update from dean

--- src/app/url.ts
+++ src/app/url.ts
@@ -1,5 +1,5 @@
 
-// let baseUrl='http://localhost:1200'
-let baseUrl='http://192.168.1.3:1200'
+ let baseUrl='http://localhost:1200'
+//let baseUrl='http://192.168.65.248:1200'
 export default baseUrl;
 

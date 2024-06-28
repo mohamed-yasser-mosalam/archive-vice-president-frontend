5c8e931c70454035a09e63f10f0b3a09bae19597
esraaelyan <esraaelyan2002@gmail.com>
2024-06-29 00:37:01 +0300

    special

--- src/app/Components/specials/update-special/update-special.component.ts
+++ src/app/Components/specials/update-special/update-special.component.ts
@@ -1,9 +1,169 @@
-import {Component, OnInit} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, NgForm} from "@angular/forms";
-import {ActivatedRoute, Router} from "@angular/router";
-import {Special} from "../../../special";
-import {SpecialService} from "../../../Services/SpecialService/special.service";
+// import {Component, OnInit} from '@angular/core';
+// import {FormArray, FormBuilder, FormGroup, NgForm} from "@angular/forms";
+// import {ActivatedRoute, Router} from "@angular/router";
+// import {Special} from "../../../special";
+// import {SpecialService} from "../../../Services/SpecialService/special.service";
+//
+//
+// @Component({
+//   selector: 'app-update-special',
+//   templateUrl: './update-special.component.html',
+//   styleUrls: ['./update-special.component.css']
+// })
+// export class UpdateSpecialComponent implements OnInit {
+//   no: any;
+//   page = this.routes.snapshot.params['id'];
+//   form: FormGroup;
+//   paths: []
+//   special: Special;
+//
+//
+//   constructor(private fb: FormBuilder, private specialService: SpecialService, private router: Router,
+//               private routes: ActivatedRoute) {
+//   }
+//
+//
+//   getSpecial() {
+//     this.specialService.getSpecialsById(this.page).subscribe((res: any) => {
+//       this.special = res;
+//       this.no = this.special.no;
+//     });
+//   }
+//
+//   onSubmit(formData: any) {
+//     this.specialService.updateSpecial(this.routes.snapshot.params['id'],formData).subscribe((result) => {
+//       this.router.navigate([`/special-pagination?page=/`, this.no])
+//     })
+//    console.log(formData)
+//   }
+//
+//   // updateSpecial() {
+//   //   this.specialService.updateSpecial(this.page, this.specialForm.value).subscribe((result) => {
+//   //     this.router.navigate([`/special-pagination?page=/`, this.no])
+//   //   })
+//   //
+//   // }
+//
+//
+//
+//   get subjects() {
+//     return this.form.get('subjects') as FormArray;
+//   }
+//
+//
+//   ngOnInit(): void {
+//     this.getSpecial();
+//     this.page = this.routes.snapshot.params['id'];
+//
+//   }
+//
+// }
+//
+// import {Component, OnInit} from '@angular/core';
+// import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
+// import {SpecialService} from "../../../Services/SpecialService/special.service";
+// import {ActivatedRoute, Router} from "@angular/router";
+// import {HttpClient} from "@angular/common/http";
+//
+// @Component({
+//   selector: 'app-update-special',
+//   templateUrl: './update-special.component.html',
+//   styleUrls: ['./update-special.component.css']
+// })
+// export class UpdateSpecialComponent implements OnInit {
+//   form: FormGroup;
+//   specialFileId: number;
+//
+//   constructor(private fb: FormBuilder, private specialService: SpecialService, private router: Router, private route: ActivatedRoute, private http: HttpClient) {
+//     this.form = this.fb.group({
+//       incomeDate: [null],
+//       sender: [''],
+//       num: [null],
+//       summary: [''],
+//       numberOfAttachments: [''],
+//       subjects: this.fb.array([this.createSubject()])
+//     });
+//   }
+//
+//   createSubject() {
+//     return this.fb.group({
+//       num: [null],
+//       head: [''],
+//       decision: this.fb.array([this.createDecision()])
+//     });
+//   }
+//
+//   createDecision() {
+//     return this.fb.group({
+//       num: [null],
+//       summary: [''],
+//       qarar: ['']
+//     });
+//   }
+//
+//   get subjects() {
+//     return this.form.get('subjects') as FormArray;
+//   }
+//
+//   addSubject() {
+//     this.subjects.push(this.createSubject());
+//   }
+//
+//   deleteSubject(index: number) {
+//     this.subjects.removeAt(index);
+//   }
+//
+//   getDecisions(subjectIndex: number) {
+//     return (this.subjects.at(subjectIndex).get('decision') as FormArray);
+//   }
+//
+//   addDecision(subjectIndex: number) {
+//     this.getDecisions(subjectIndex).push(this.createDecision());
+//   }
+//
+//   deleteDecision(subjectIndex: number, decisionIndex: number) {
+//     this.getDecisions(subjectIndex).removeAt(decisionIndex);
+//   }
+//
+//   updateSpecialFile(data) {
+//     this.specialService.updateSpecial(this.specialFileId, data).subscribe(() => {
+//       this.router.navigate(['/special-files']);
+//     });
+//   }
+//
+//   loadSpecialFile() {
+//     this.specialFileId = this.route.snapshot.params['id'];
+//     this.specialService.getSpecialsById(this.specialFileId).subscribe((data: any) => {
+//       this.form.patchValue({
+//         incomeDate: data.incomeDate,
+//         sender: data.sender,
+//         num: data.num,
+//         summary: data.summary,
+//         numberOfAttachments: data.numberOfAttachments
+//       });
+//       this.form.setControl('subjects', this.fb.array(data.subjects.map(subject => this.fb.group({
+//         num: subject.num,
+//         head: subject.head,
+//         decision: this.fb.array(subject.decision.map(decision => this.fb.group({
+//           num: decision.num,
+//           summary: decision.summary,
+//           qarar: decision.qarar
+//         })))
+//       }))));
+//     });
+//   }
+//
+//   ngOnInit(): void {
+//     this.loadSpecialFile();
+//   }
+// }
 
+
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from "@angular/forms";
+import { SpecialService } from "../../../Services/SpecialService/special.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-update-special',
@@ -11,51 +171,101 @@ import {SpecialService} from "../../../Services/SpecialService/special.service";
   styleUrls: ['./update-special.component.css']
 })
 export class UpdateSpecialComponent implements OnInit {
-  no: any;
-  page = this.routes.snapshot.params['id'];
   form: FormGroup;
-  paths: []
-  special: Special;
+  specialFileId: number;
 
-
-  constructor(private fb: FormBuilder, private specialService: SpecialService, private router: Router,
-              private routes: ActivatedRoute) {
-  }
-
-
-  getSpecial() {
-    this.specialService.getSpecialsById(this.page).subscribe((res: any) => {
-      this.special = res;
-      this.no = this.special.no;
+  constructor(
+    private fb: FormBuilder,
+    private specialService: SpecialService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {
+    this.form = this.fb.group({
+      incomeDate: [null],
+      sender: [''],
+      num: [null],
+      summary: [''],
+      numberOfAttachments: [''],
+      subjects: this.fb.array([])
     });
   }
 
-  onSubmit(formData: any) {
-    this.specialService.updateSpecial(this.routes.snapshot.params['id'],formData).subscribe((result) => {
-      this.router.navigate([`/special-pagination?page=/`, this.no])
-    })
-   console.log(formData)
+  createSubject(subjectData?): FormGroup {
+    return this.fb.group({
+      num: [subjectData?.num || null],
+      head: [subjectData?.head || ''],
+      decision: this.fb.array(subjectData?.decision ? subjectData.decision.map(decisionData => this.createDecision(decisionData)) : [])
+    });
   }
 
-  // updateSpecial() {
-  //   this.specialService.updateSpecial(this.page, this.specialForm.value).subscribe((result) => {
-  //     this.router.navigate([`/special-pagination?page=/`, this.no])
-  //   })
-  //
-  // }
+  createDecision(decisionData?): FormGroup {
+    return this.fb.group({
+      qarar: [decisionData?.qarar || ''],
+      summary: [decisionData?.summary || '']
+    });
+  }
 
-
-
-  get subjects() {
+  get subjects(): FormArray {
     return this.form.get('subjects') as FormArray;
   }
 
-
-  ngOnInit(): void {
-    this.getSpecial();
-    this.page = this.routes.snapshot.params['id'];
-
+  addSubject(subjectData?): void {
+    this.subjects.push(this.createSubject(subjectData));
   }
 
+  deleteSubject(index: number): void {
+    this.subjects.removeAt(index);
+  }
+
+  getDecisions(subjectIndex: number): FormArray {
+    return this.subjects.at(subjectIndex).get('decision') as FormArray;
+  }
+
+  addDecision(subjectIndex: number, decisionData?): void {
+    this.getDecisions(subjectIndex).push(this.createDecision(decisionData));
+  }
+
+  deleteDecision(subjectIndex: number, decisionIndex: number): void {
+    this.getDecisions(subjectIndex).removeAt(decisionIndex);
+  }
+
+  updateSpecialFile(): void {
+    if (this.form.valid) {
+      const formData = this.form.value;
+      this.specialService.updateSpecial(this.specialFileId, formData).subscribe(() => {
+        this.router.navigate(['/special-files']);
+      });
+    } else {
+      // Handle form validation errors if needed
+    }
+  }
+
+  loadSpecialFile(): void {
+    this.specialFileId = this.route.snapshot.params['id'];
+    this.specialService.getSpecialsById(this.specialFileId).subscribe((data: any) => {
+      this.form.patchValue({
+        incomeDate: data.incomeDate,
+        sender: data.sender,
+        num: data.num,
+        summary: data.summary,
+        numberOfAttachments: data.numberOfAttachments
+      });
+
+      // Clear existing subjects and decisions
+      this.subjects.clear();
+
+      // Add subjects and decisions from the fetched data
+      data.subjects.forEach(subjectData => {
+        const subjectFormGroup = this.createSubject(subjectData);
+        this.subjects.push(subjectFormGroup);
+      });
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadSpecialFile();
+  }
 }
+
 

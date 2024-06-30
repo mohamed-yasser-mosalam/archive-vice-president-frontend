93f643484ed0ec6c120fc87d32e7dac59e4eb924
esraaelyan <esraaelyan2002@gmail.com>
2024-07-01 02:27:13 +0300

    addMandate

--- src/app/Components/Mandate/add-mandate/add-mandate.component.ts
+++ src/app/Components/Mandate/add-mandate/add-mandate.component.ts
@@ -1,9 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {ExportServiceService} from "../../../Services/ExportsServices/export-service.service";
-import {Router} from "@angular/router";
-import {HttpClient} from "@angular/common/http";
-import {MandateService} from "../../../Services/mandate/mandate.service";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MandateService } from '../../../Services/mandate/mandate.service';
 
 @Component({
   selector: 'app-add-mandate',
@@ -11,41 +8,38 @@ import {MandateService} from "../../../Services/mandate/mandate.service";
   styleUrls: ['./add-mandate.component.css']
 })
 export class AddMandateComponent implements OnInit {
-  numberOfMandate: any;
-  exportForm: FormGroup;
+  mandateForm: FormGroup;
 
-
-  constructor(private mandateService: MandateService, private router: Router,
-              private fb: FormBuilder, private http: HttpClient) {}
-  ngOnInit(): void {
-    this.getMandateCount();
-    this.exportForm = this.fb.group({
-      receiver: ['', [Validators.required, Validators.minLength(4)]],
-      summary: ['', [Validators.required, Validators.minLength(4)]],
-      date: ['', [Validators.required, Validators.minLength(4)]],
-      no: [''],
-      recipientName: [''],
-      num: ['', [Validators.required, Validators.minLength(1)]],
-      numberOfAttachments: ['', [Validators.required, Validators.minLength(1)]],
-      recipientDate: [''],
+  constructor(private formBuilder: FormBuilder, private mandateService: MandateService) {
+    this.mandateForm = this.formBuilder.group({
+      department: ['', Validators.required],
+      name: ['', Validators.required],
+      degree: ['', Validators.required],
+      deputationUniversity: ['', Validators.required],
+      deputationPeriod: ['', Validators.required],
+      deputationDays: this.formBuilder.group({
+         id:['']
+      }),
+      departmentAccept: ['', Validators.required],
+      departmentDate: ['', Validators.required],
+      departmentRecordNum: ['', Validators.required],
+      facultyRecordAccepts: ['', Validators.required],
+      facultyDate: ['', Validators.required],
+      facultyNum: ['', Validators.required],
+      universityAccept: ['', Validators.required],
+      universityDate: ['', Validators.required],
+      universityNumber: ['', Validators.required],
+      notes: ['']
     });
   }
 
-  addExportFile(data: any) {
-    this.mandateService.addMandate(data).subscribe(() => {
-      window.location.reload();
-    });
-  }
+  ngOnInit() {}
 
-  getMandateCount(): void {
-    this.mandateService.getAllMandateNumbers().subscribe((numberOfMandate: any) => {
-      this.numberOfMandate = numberOfMandate + 1;
-    });
-  }
-
-  onSubmit(): void {
-    if (this.exportForm.valid) {
-      this.addExportFile(this.exportForm.value);
+  onSubmit() {
+    if (this.mandateForm.valid) {
+      const formData = this.mandateForm.value;
+      this.mandateService.addMandate(formData).subscribe(response => {}, );
+      console.log( formData);
     }
   }
 }
